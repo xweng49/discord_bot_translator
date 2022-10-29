98dad77f5aaeb7a92b977b18778dea5fbcc3f038
Xing Weng <xing@xings-air.lan>
2022-10-29 01:05:52 -0500

    changed to use slash command

--- bot.py
+++ bot.py
@@ -1,34 +1,64 @@
-import discord
-import os
+import interactions
+from language_dict import language_dict
 from googletrans import Translator
 
-intents = discord.Intents.all()
-
-client = discord.Client(intents=intents)
-token = os.environ['token']
+from cred import token, guildId
 
 translator = Translator()
+user_language = {'global':'ja'}
 
-@client.event
-async def on_ready():
-    print(f'We have logged in as {client.user}')
+bot = interactions.Client(token=token)
 
-@client.event
-async def on_message(message):
-    if message.author==client.user:
-        return
+@bot.event()
+async def on_start():
+    print(f'We have logged in as {bot.me.name}')
 
-    if message.content.startswith('!ts'):
-        origin_message = message.content[3:].strip()
-        
-        if origin_message:
-            translation = translator.translate(origin_message, src='en', dest='ja')
-            await message.channel.send(f'{translation.text} \n{translation.pronunciation}')
-        else:
-            await message.channel.send(f'Nothing to translate')
+"""Translating Text"""
+@bot.command(description="Translate Text")
+@interactions.option(name='text', description="Translate Text")
+async def ts(ctx: interactions.CommandContext, option: str):
+    user = ctx.author.user.id
+    if user in user_language.keys():
+        language = language_dict[user_language[user]]
+        translation = translator.translate(option, src='en', dest=language)
+    else:
+        language = language_dict[user_language['global']]
+        translation = translator.translate(option, src='en', dest=language)
+    await ctx.send(f"{translation.text}\n{translation.pronunciation}")
 
-        
-        
-        
+"""Translator bot settings"""
+@bot.command(description="Translator bot settings")
+async def ts_settings(ctx: interactions.CommandContext):
+    """This description isn't seen in UI (yet?)"""
+    pass
 
-client.run(token)
+"""Change global translation option"""
+@ts_settings.subcommand(description='Change global translation option')
+@interactions.option(name='option', description="Set global language translation")
+async def global_language(ctx: interactions.CommandContext, option: str):
+    if option in language_dict.keys():
+        user_language['global']=option
+        await ctx.send(f"Setting global translation to: {language_dict[option]}")
+    else:
+        await ctx.send(f"Language Option not available")
+
+"""Change user translation option"""
+@ts_settings.subcommand(description="Change user translation option")
+@interactions.option(name='option', description="Set user language translation, use 0 to reset user language")
+async def set_user_language(ctx: interactions.CommandContext, option: str):
+    if option in language_dict.keys():
+        user = ctx.author.user.id
+        user_language[user]=option
+        await ctx.send(f"Setting {ctx.author}'s translation to: {language_dict[option]}")
+    elif option=='0':
+        user_language.pop(user)
+    else:
+        await ctx.send(f"Language Option not available")
+
+"""List all language options"""
+@ts_settings.subcommand(description="List all language options")
+async def list_language_options(ctx: interactions.CommandContext):
+    await ctx.send('\n'.join([f'{key} : {val}' for key, val in language_dict.items()]))
+
+
+bot.start()
